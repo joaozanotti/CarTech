65c10a0ee3342ff550ee9b8458a453199f786580
joaozanotti <joaovzanotti@gmail.com>
2023-12-10 16:47:21 -0300

    Adicionando ícone na aba das páginas

--- control/funcoes.php
+++ control/funcoes.php
@@ -8,6 +8,7 @@ function criaHeader($titulo) {
                 <title>'.$titulo.' - CarTech</title>
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
+                '.criarIcone($titulo).'
                 <style>
                     .bi {
                         font-size: 20px;
@@ -43,6 +44,14 @@ function criaHeader($titulo) {
                     </div>
                     '.criarNav($titulo).'
                 </header>';
+}
+
+function criarIcone($titulo) {
+    if ($titulo == "Home") {
+        return '<link rel="icon" type="image/x-icon" href="../img/icone.png">';
+    } else {
+        return '<link rel="icon" type="image/x-icon" href="../../img/icone.png">';
+    }
 }
 
 function criarLinks($titulo) {
